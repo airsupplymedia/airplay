dcf464d9c28913a65e22eb53471a2be4e4908035
jochenschmich <github@airsupply.de>
2013-09-17 09:28:55 +0200

    Added convenience function for query result conversion to Functions

--- airplay-core/src/main/java/de/airsupply/commons/core/util/Functions.java
+++ airplay-core/src/main/java/de/airsupply/commons/core/util/Functions.java
@@ -1,6 +1,7 @@
 package de.airsupply.commons.core.util;
 
 import org.neo4j.graphdb.Node;
+import org.springframework.data.neo4j.support.Neo4jTemplate;
 
 import de.airsupply.airplay.core.model.PersistentNode;
 import de.airsupply.commons.core.util.CollectionUtils.Function;
@@ -24,6 +25,19 @@ public class Functions {
 			@Override
 			public Long apply(PersistentNode source) {
 				return source.getIdentifier();
+			}
+
+		};
+	}
+
+	public static <T extends PersistentNode> Function<Object, T> toPersistentState(final Neo4jTemplate neo4jTemplate,
+			final Class<?> persistentNodeClass) {
+		return new Function<Object, T>() {
+
+			@SuppressWarnings("unchecked")
+			@Override
+			public T apply(Object source) {
+				return (T) neo4jTemplate.convert(source, persistentNodeClass);
 			}
 
 		};
