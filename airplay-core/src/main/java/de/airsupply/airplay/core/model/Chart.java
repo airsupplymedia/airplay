8291d5ea85b1e81cc281da9144742086468cfec1
jochenschmich <github@airsupply.de>
2013-10-28 20:11:56 +0100

    JSON serialization must not transfer ChartState-objects of each Chart object

--- airplay-core/src/main/java/de/airsupply/airplay/core/model/Chart.java
+++ airplay-core/src/main/java/de/airsupply/airplay/core/model/Chart.java
@@ -38,6 +38,7 @@ public class Chart extends PersistentNode {
 		this.name = name;
 	}
 
+	@JsonIgnore
 	public List<ChartState> getChartStateList() {
 		if (chartStates != null) {
 			return CollectionUtils.asList(chartStates);
