1e66528489011e1fd907e050f6be81c34792fc94
jochenschmich <github@airsupply.de>
2013-10-30 08:15:54 +0100

    Introduction of facility methods for alert handling

--- airplay-web/src/main/webapp/js/commons/utils.js
+++ airplay-web/src/main/webapp/js/commons/utils.js
@@ -1,3 +1,19 @@
 function toFirstLower(string) {
 	return string.charAt(0).toLowerCase() + string.slice(1);
 }
+
+function createAlert(message, type, scope, timeout, duration) {
+	if (duration == null) {
+		duration = 3000;
+	}
+	if (scope.alerts == null) {
+		scope.alerts = [];
+	}
+	scope.alerts.unshift({
+		type : type,
+		message : message
+	});
+	timeout(function() {
+		scope.alerts.pop();
+	}, duration);
+}
