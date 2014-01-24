2a2214ea1c5a1a18b28c0fdc3eb016f7e9adcd17
jochenschmich <github@airsupply.de>
2014-01-24 21:08:05 +0100

    Performed clean up on Configuration classes

--- airplay-web/src/main/java/de/airsupply/airplay/web/config/WebConfiguration.java
+++ airplay-web/src/main/java/de/airsupply/airplay/web/config/WebConfiguration.java
@@ -1,12 +1,10 @@
 package de.airsupply.airplay.web.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import de.airsupply.airplay.core.config.ApplicationConfiguration;
 
@@ -15,13 +13,4 @@ import de.airsupply.airplay.core.config.ApplicationConfiguration;
 @EnableWebMvc
 @ComponentScan("de.airsupply.airplay.web.controller")
 public class WebConfiguration extends WebMvcConfigurerAdapter {
-
-	@Bean
-	public InternalResourceViewResolver viewResolver() {
-		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-		resolver.setPrefix("/WEB-INF/view/");
-		resolver.setSuffix(".jsp");
-		return resolver;
-	}
-
 }
