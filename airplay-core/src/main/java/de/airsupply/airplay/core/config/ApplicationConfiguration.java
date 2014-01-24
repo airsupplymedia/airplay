95c334d0604041b8af493f804a045fdfe3629689
jochenschmich <github@airsupply.de>
2014-01-24 15:21:06 +0100

    Enabled transaction management to activate write operations from Controller instances

--- airplay-core/src/main/java/de/airsupply/airplay/core/config/ApplicationConfiguration.java
+++ airplay-core/src/main/java/de/airsupply/airplay/core/config/ApplicationConfiguration.java
@@ -3,8 +3,10 @@ package de.airsupply.airplay.core.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
+@EnableTransactionManagement
 @Import(DatabaseConfiguration.class)
 @ComponentScan({ "de.airsupply.airplay.core", "de.airsupply.commons.core" })
 public class ApplicationConfiguration {
